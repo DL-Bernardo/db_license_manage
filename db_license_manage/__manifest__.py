9e4caca09c0b97eae247084112ef5fb59a6251ef
Bernardo Vieira <bvieira@digitalub.ao>
2025-12-06 21:27:11 +0100

    Fix images path for Odoo Apps

--- db_license_manage/__manifest__.py
+++ db_license_manage/__manifest__.py
@@ -1,10 +1,9 @@
-# db_license_manager/__manifest__.py
 {
     'name': 'Database License Manager - DIGITALUB',
     'version': '17.0.1.0.1',
     'category': 'Tools',
     'summary': 'Software License Management with Automatic Blocking',
-    'author': 'Digitalub',
+    'author': 'DIGITALUB ANGOLA',
     'depends': ['base', 'web', 'website', 'auth_signup', 'mail'],
     'external_dependencies': {
         'python': ['pyjwt', 'cryptography'],
@@ -28,7 +27,15 @@
     'installable': True,
     'application': True,
     'license': 'OPL-1',
-    'price': 385,
+    'price': 400,
     'currency': 'USD',
-    'images': ['static/description/banner.png']
+    'images': [
+        'db_license_manage/static/description/banner.png',
+        'db_license_manage/static/description/01.licenca_nao_encontrada.png',
+        'db_license_manage/static/description/02.configurar_licenca.png',
+        'db_license_manage/static/description/03.licenca_valida.png',
+        'db_license_manage/static/description/04.Aviso_5_dias_licenca1.png',
+        'db_license_manage/static/description/05.Aviso_5_dias_licenca.png',
+        'db_license_manage/static/description/06.Licenca_expirada.png',
+    ],
 }
